02947124967bd33ae3d8ff41e4fcbfe41680c680
Fredrik Oterholt <fredrik@technologic.as>
2025-09-30 14:19:06 +0200

    Reduce to map and get values

---
+++ lib/mapEnum.ts
@@ -0,0 +1,6 @@
+export type ValueOf<T> = T[keyof T];
+
+export function getEnumKeyByEnumValue<T extends Record<string, string | number>>(myEnum: T, enumValue: ValueOf<T> | string): keyof T | undefined {
+  const keys = Object.keys(myEnum).filter((x) => myEnum[x] === enumValue);
+  return keys.length > 0 ? keys[0] : undefined;
+}
