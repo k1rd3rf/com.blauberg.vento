c0e47697236ebb9768f290ab52c058d775ad5dd0
Fredrik Oterholt <fredrik@technologic.as>
2025-10-06 21:10:34 +0200

    Add enum for capabilities

---
+++ lib/capabilities.ts
@@ -0,0 +1,33 @@
+/* eslint-disable camelcase */
+
+export enum Capabilities {
+  alarm_connectivity = 'alarm_connectivity',
+  onoff = 'onoff',
+  fan_speed = 'fan_speed',
+  measure_humidity = 'measure_humidity',
+
+  alarm_boost = 'alarm_boost',
+  alarm_filter = 'alarm_filter',
+  alarm_generic = 'alarm_generic',
+  filter_timer = 'filter_timer',
+  manualSpeed = 'manualSpeed',
+  measure_RPM = 'measure_RPM',
+  operationMode = 'operationMode',
+  speedMode = 'speedMode',
+  timerMode = 'timerMode',
+  timerMode_timer = 'timerMode_timer',
+}
+
+export enum ActionCards {
+  manualSpeed_set = 'manualSpeed_set',
+  operation_mode = 'operation_mode',
+  speed_mode = 'speed_mode',
+  timer_mode = 'timer_mode',
+}
+
+export enum DeviceSettings {
+  unit_type = 'unit_type',
+  humidity_sensor = 'humidity_sensor',
+  humidity_threshold = 'humidity_threshold',
+  boost_delay = 'boost_delay',
+}
