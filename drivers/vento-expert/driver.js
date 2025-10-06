288abb7dd787c1ea7f88f2be51a20c8fe4b8a41b
Fredrik Oterholt <fredrik@technologic.as>
2025-10-06 21:33:28 +0200

    Clean up driver before => ts

--- drivers/vento-expert/driver.js
+++ drivers/vento-expert/driver.js
@@ -24,101 +24,86 @@ class VentoDriver extends Driver {
   }
 
   // eslint-disable-next-line camelcase
-  start_discover_loop() {
+  start_discover_loop = () => {
     this._timer = this.homey.setInterval(async () => {
       await this.locateDevices();
     }, 10000);
-  }
+  };
 
-  async setDeviceValue(device, devicepass, param, value) {
-    const packet = new Packet(device.id, devicepass, FunctionType.WRITE, [
-      DataEntry.of(param, value),
-    ]);
-    return this.modbusClient.send(packet, device.ip).then((result) => result);
-  }
+  send = (packet, ip) =>
+    this.modbusClient.send(packet, ip).then(mapModbusResponse);
 
-  async setOnoffStatus(device, devicepass, value) {
-    return this.setDeviceValue(device, devicepass, Parameter.ON_OFF, value);
-  }
-
-  async setSpeedMode(device, devicepass, value) {
-    return this.setDeviceValue(device, devicepass, Parameter.SPEED, value);
-  }
-
-  async setOperationMode(device, devicepass, value) {
-    return this.setDeviceValue(
-      device,
-      devicepass,
-      Parameter.VENTILATION_MODE,
-      value
+  getDeviceState = async (device, devicepass) =>
+    this.send(
+      new Packet(device.id, devicepass, FunctionType.READ, [
+        DataEntry.of(Parameter.ON_OFF),
+        DataEntry.of(Parameter.SPEED),
+        DataEntry.of(Parameter.MANUAL_SPEED),
+        DataEntry.of(Parameter.BOOT_MODE),
+        DataEntry.of(Parameter.BOOST_MODE_DEACTIVATION_DELAY),
+        DataEntry.of(Parameter.VENTILATION_MODE),
+        DataEntry.of(Parameter.FILTER_ALARM),
+        DataEntry.of(Parameter.FILTER_TIMER),
+        DataEntry.of(Parameter.CURRENT_HUMIDITY),
+        DataEntry.of(Parameter.HUMIDITY_SENSOR_ACTIVATION),
+        DataEntry.of(Parameter.HUMIDITY_THRESHOLD),
+        DataEntry.of(Parameter.UNIT_TYPE),
+        DataEntry.of(Parameter.FAN1RPM),
+        DataEntry.of(Parameter.TIMER_MODE),
+        DataEntry.of(Parameter.READ_ALARM),
+        DataEntry.of(11), // Active timer countdown
+      ]),
+      device.ip
     );
-  }
 
-  async setTimerMode(device, devicepass, value) {
-    return this.setDeviceValue(device, devicepass, Parameter.TIMER_MODE, value);
-  }
-
-  async setManualSpeed(device, devicepass, value) {
-    return this.setDeviceValue(
-      device,
-      devicepass,
-      Parameter.MANUAL_SPEED,
-      value
+  setDeviceValue = async (device, devicepass, param, value) =>
+    this.modbusClient.send(
+      new Packet(device.id, devicepass, FunctionType.WRITE, [
+        DataEntry.of(param, value),
+      ]),
+      device.ip
     );
-  }
 
-  async setHumiditySensor(device, devicepass, value) {
-    return this.setDeviceValue(
+  setOnoffStatus = async (device, devicepass, value) =>
+    this.setDeviceValue(device, devicepass, Parameter.ON_OFF, value);
+
+  setSpeedMode = async (device, devicepass, value) =>
+    this.setDeviceValue(device, devicepass, Parameter.SPEED, value);
+
+  setOperationMode = async (device, devicepass, value) =>
+    this.setDeviceValue(device, devicepass, Parameter.VENTILATION_MODE, value);
+
+  setTimerMode = async (device, devicepass, value) =>
+    this.setDeviceValue(device, devicepass, Parameter.TIMER_MODE, value);
+
+  setManualSpeed = async (device, devicepass, value) =>
+    this.setDeviceValue(device, devicepass, Parameter.MANUAL_SPEED, value);
+
+  setHumiditySensor = async (device, devicepass, value) =>
+    this.setDeviceValue(
       device,
       devicepass,
       Parameter.HUMIDITY_SENSOR_ACTIVATION,
       value
     );
-  }
 
-  async setHumiditySensorThreshold(device, devicepass, value) {
-    return this.setDeviceValue(
+  setHumiditySensorThreshold = async (device, devicepass, value) =>
+    this.setDeviceValue(
       device,
       devicepass,
       Parameter.HUMIDITY_THRESHOLD,
       value
     );
-  }
 
-  async setBoostDelay(device, devicepass, value) {
-    return this.setDeviceValue(
+  setBoostDelay = async (device, devicepass, value) =>
+    this.setDeviceValue(
       device,
       devicepass,
       Parameter.BOOST_MODE_DEACTIVATION_DELAY,
       value
     );
-  }
 
-  async getDeviceState(device, devicepass) {
-    // Assemble package for reading ON_OFF state
-    const packet = new Packet(device.id, devicepass, FunctionType.READ, [
-      DataEntry.of(Parameter.ON_OFF),
-      DataEntry.of(Parameter.SPEED),
-      DataEntry.of(Parameter.MANUAL_SPEED),
-      DataEntry.of(Parameter.BOOT_MODE),
-      DataEntry.of(Parameter.BOOST_MODE_DEACTIVATION_DELAY),
-      DataEntry.of(Parameter.VENTILATION_MODE),
-      DataEntry.of(Parameter.FILTER_ALARM),
-      DataEntry.of(Parameter.FILTER_TIMER),
-      DataEntry.of(Parameter.CURRENT_HUMIDITY),
-      DataEntry.of(Parameter.HUMIDITY_SENSOR_ACTIVATION),
-      DataEntry.of(Parameter.HUMIDITY_THRESHOLD),
-      DataEntry.of(Parameter.UNIT_TYPE),
-      DataEntry.of(Parameter.FAN1RPM),
-      DataEntry.of(Parameter.TIMER_MODE),
-      DataEntry.of(Parameter.READ_ALARM),
-      DataEntry.of(11), // Active timer countdown
-    ]);
-    // Send package and wait for response.
-    return this.modbusClient.send(packet, device.ip).then(mapModbusResponse);
-  }
-
-  async locateDevices() {
+  locateDevices = async () => {
     const locatedDevices = await this.modbusClient.findDevices();
     const oldamount = this.deviceList.length;
     this.log(
@@ -156,33 +141,29 @@ class VentoDriver extends Driver {
         this.log('Not getting the state since device is not available yet');
       }
     });
-  }
+  };
 
-  locateDeviceById(id) {
-    return this.deviceList.find((e) => {
+  locateDeviceById = (id) =>
+    this.deviceList.find((e) => {
       return e.id === id;
     });
-  }
 
-  async onPair(session) {
+  onPair = async (session) => {
     session.setHandler('list_devices', async (data) => {
       this.log('Provide user list of discovered Vento fans to choose from.');
       this.log('Start discovery of Vento Expert devices on the local network');
       await this.locateDevices();
-      this.log(JSON.stringify(this.deviceList));
       this.log(`Located [${this.deviceList.length}] Vento expert devices`);
-      // Lets return the mapped list
       return this.deviceList.map((device) => {
-        this.log(JSON.stringify(device));
-        const ventodevice = {
+        const ventoDevice = {
           id: device.id,
           name: `Vento Expert ${device.id}`,
           data: {
             id: device.id,
           },
         };
-        this.log(`located: ${JSON.stringify(ventodevice)}`);
-        return ventodevice;
+        this.log(`located: ${JSON.stringify(ventoDevice)}`);
+        return ventoDevice;
       });
     });
 
@@ -192,7 +173,7 @@ class VentoDriver extends Driver {
         this.log(`Vento fan [${data[0].name}] added`);
       } else this.log('no Vento fan added');
     });
-  }
+  };
 }
 
 module.exports = VentoDriver;
